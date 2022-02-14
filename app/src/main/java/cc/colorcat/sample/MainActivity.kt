1614b06e42519d16eb66e38bc6b58061b2fa04ab
ccolorcat <ch.xx@outlook.com>
2022-02-15 00:26:40 +0800

    upgrade sample

--- app/src/main/java/cc/colorcat/sample/MainActivity.kt
+++ app/src/main/java/cc/colorcat/sample/MainActivity.kt
@@ -2,10 +2,18 @@ package cc.colorcat.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import cc.colorcat.xlogger.XLogger
 
 class MainActivity : AppCompatActivity() {
+    private val logger by lazy(LazyThreadSafetyMode.NONE) { XLogger.getLogger(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.test).setOnClickListener {
+            logger.d { "this is a test log." }
+        }
     }
 }
